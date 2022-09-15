28b788635c34da47116a3716b6848aa2c04c52b7
kgrebenyuk <kgrebenyuk@luxoft.com>
2022-09-15 17:02:19 +0300

    dz5

--- src/My_Package/MyClass.java
+++ src/My_Package/MyClass.java
@@ -1,13 +1,17 @@
 package My_Package;
 
+import java.util.Scanner;
+
 public class MyClass {
     public static void main(String[] args) {
 
-        double a = 5, b = 6, h = 10;
-        double volume = a * b * h;
-        double length = 4 * (a + b + h);
-
-        System.out.println("Об'єм паралелепіпеда = " + volume);
-        System.out.println("Cума довжин всіх ребер паралелепіпеда = " + length);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Калькулятор вычисления суммы двух целых чисел :)");
+        System.out.println("Введите первое число");
+        int num_1 = sc.nextInt();
+        System.out.println("Введите второе число");
+        int num_2 = sc.nextInt();
+        int sum = num_1 + num_2;
+        System.out.println(num_1 + " + " + num_2 + " = " + sum);
     }
 }
