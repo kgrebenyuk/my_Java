f6c66354d383d7b1f8d1cc3687b08771f8a9d31a
kgrebenyuk <kgrebenyuk@luxoft.com>
2022-09-15 16:22:10 +0300

    commit2

---
+++ src/My_Package/MyClass.java
@@ -0,0 +1,10 @@
+package My_Package;
+
+public class MyClass {
+    public static void main(String[] args) {
+
+        double longtitude = 48.85827854767629, lattitude  = 2.294496409590613;
+        System.out.println("Position of Tour Eiffel is: (" + longtitude + ", " + lattitude +  ")");
+    }
+
+}
