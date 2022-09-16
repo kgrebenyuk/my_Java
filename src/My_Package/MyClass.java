61173c46cf53ed4cb96dc886a1e53d9742871f94
kgrebenyuk <kgrebenyuk@luxoft.com>
2022-09-16 16:04:20 +0300

    ДЗ 8. Вгадай число

--- src/My_Package/MyClass.java
+++ src/My_Package/MyClass.java
@@ -1,16 +1,27 @@
 package My_Package;
+
 import java.util.Scanner;
+import java.util.Random;
 
 public class MyClass {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Калькулятор вычисления суммы двух целых чисел :)");
-        System.out.println("Введите первое число");
-        int num_1 = sc.nextInt();
-        System.out.println("Введите второе число");
-        int num_2 = sc.nextInt();
-        int sum = num_1 + num_2;
-        System.out.println(num_1 + " + " + num_2 + " = " + sum);
+        Random rd = new Random();
+        int in_number, random_mumber;
+
+        while (true) {
+            System.out.println("Введите целое число от 1 до 6 ( 0 - выход)");
+            in_number = sc.nextInt();
+            random_mumber = rd.nextInt(6) + 1;     // (0..5) + 1
+            if (in_number == 0) break;
+            if (in_number < 1 || in_number > 6) continue;
+            if (in_number == random_mumber) {
+                System.out.println("Угадал :), число: " + random_mumber);
+            } else {
+                System.out.println("Не угадал, число: " + random_mumber);
+            }
+        }
+        System.out.println("Удачи :)");
     }
 }
