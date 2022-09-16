7783a08bb0004649ece62ecf52cfb20edcb90619
kgrebenyuk <kgrebenyuk@luxoft.com>
2022-09-16 17:58:52 +0300

    ДЗ 7. Counter Strike

--- src/My_Package/MyClass.java
+++ src/My_Package/MyClass.java
@@ -1,27 +1,32 @@
 package My_Package;
 
 import java.util.Scanner;
-import java.util.Random;
+import java.lang.String;
 
 public class MyClass {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        Random rd = new Random();
-        int in_number, random_mumber;
+        float[] score = new float[2];  //нулевые default значения
+        String[] name = new String[2];
 
-        while (true) {
-            System.out.println("Введите целое число от 1 до 6 ( 0 - выход)");
-            in_number = sc.nextInt();
-            random_mumber = rd.nextInt(6) + 1;     // (0..5) + 1
-            if (in_number == 0) break;
-            if (in_number < 1 || in_number > 6) continue;
-            if (in_number == random_mumber) {
-                System.out.println("Угадал :), число: " + random_mumber);
-            } else {
-                System.out.println("Не угадал, число: " + random_mumber);
+        System.out.println("Введите имя команды № 1");
+        name[0] = sc.nextLine();
+        System.out.println("Введите имя команды № 2");
+        name[1] = sc.nextLine();
+
+        for (int j = 0; j < 2; j++) {
+            for (int i = 1; i < 6; i++) {
+                System.out.println("Введите рузультат " + i + " игрока комманды № " + (j + 1));
+                score[j] += sc.nextFloat();
             }
+            score[j] /= 5; // среднее из 5 по j-й команде
         }
-        System.out.println("Удачи :)");
+        if (score[0] > score[1])
+            System.out.println("Перемогла команда '" + name[0] + "' набрала " + score[0] + " очків");
+        else if (score[0] < score[1])
+            System.out.println("Перемогла команда '" + name[1] + "' набрала " + score[1] + " очків");
+        else
+            System.out.println("Нiчья мiж '" + name[0] + "' и '" + name[1] + "' " + score[0] + " / " + score[1]);
     }
 }
