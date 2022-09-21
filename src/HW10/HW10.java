3609cc608e17e556f8784d23d3e9f54bbec668f3
kgrebenyuk <kgrebenyuk@luxoft.com>
2022-09-21 15:05:05 +0300

    HW10 ver 2

--- src/HW10/HW10.java
+++ src/HW10/HW10.java
@@ -2,7 +2,6 @@ package HW10;
 
 public class HW10 {
     public static void main(String[] args) {
-
         int num = 1, i = 1;
         System.out.println("Список 100 номеров без цифр '4' и '9'");
         while (num < 100) {
